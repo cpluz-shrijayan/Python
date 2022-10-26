fe70834d6d07aeb1854dabc86b31fc654122902d
Shrijayan Rajendran <shrijayan@cpluz.com>
2022-10-26 02:31:37 -0700

    Create Which Mixture.py

---
+++ CodeChef/Which Mixture.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    a,b=map(int,input().split())
+    if a>0 and b>0: 
+         print("Solution")
+    elif b==0:
+        print("Solid")
+    else:
+        print("Liquid")
