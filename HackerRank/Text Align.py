219a3d4f187d082fd0486c6a2774bcbc3848cc8b
Shrijayan Rajendran <shrijayan@cpluz.com>
2022-11-19 12:00:00 +0000

    A month ago

---
+++ HackerRank/Text Align.py
@@ -0,0 +1,14 @@
+#Replace all ______ with rjust, ljust or center. 
+
+thickness = int(input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+…
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
