3220400ae925f8bb29542118f2791835606f84b3
Shrijayan Rajendran <shrijayan@cpluz.com>
2022-11-20 12:00:00 +0000

    A month ago

---
+++ HackerRank/Set Mutations.py
@@ -0,0 +1,15 @@
+na=int(input())
+a=set(map(int,input().split()))
+nb=int(input())
+for i in range(nb):
+    s=list(input().split())
+    l=set(map(int,input().split()))
+    if(s[0]=='update'):
+        a.update(l)
+    elif(s[0]=='intersection_update'):
+        a.intersection_update(l)
+    elif(s[0]=='difference_update'):
+        a.difference_update(l)
+    else:
+        a.symmetric_difference_update(l)
+print(sum(a))
